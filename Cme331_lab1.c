26d7bde3af6abe84e3f790c9ff8a0cf56e366b84
Parazyt <113295033+Parzyt-bit@users.noreply.github.com>
2023-09-19 08:41:21 -0600

    Add files via upload

---
+++ Cme331_lab1.c
@@ -0,0 +1,57 @@
+
+#define SYSCTL_RCGC2   (* ((volatile unsigned long *) 0x400FE108))  // 0x400FE108 is a 'memory address' of a register that has particular function in the 'microcontroller'
+#define GPIO_PORTF_DEN (* ((volatile unsigned long *) 0x4002551C))
+#define GPIO_PORTF_DIR (* ((volatile unsigned long *) 0x40025400))
+#define GPIO_PORTF_AFSEL (* ((volatile unsigned long *) 0x40025420))
+#define GPIO_PORTF_DATA (* ((volatile unsigned long *) 0x400253FC))
+#define GPIO_PORTF_PUR (* ((volatile unsigned long *) 0x40025510))
+
+void gpio_portf_init(void)
+{
+    // This line sets the second bit (bit 3) high in the register at location 0x400FE108
+    SYSCTL_RCGC2 |= 0x20; // enables 'clock' for PortF
+    int delay = SYSCTL_RCGC2;
+
+    // Configuration for Red LED
+    GPIO_PORTF_DEN |= 0x02;  // set PF1 to be a digital pin
+    GPIO_PORTF_DIR |= 0x02; // set PF1 to be an output pin
+    GPIO_PORTF_AFSEL &= ~0x02; // disable alternate functions for PF1
+    GPIO_PORTF_DATA |= 0x02;
+
+   // Configuration for Blue LED
+   GPIO_PORTF_DEN |= 0x04;  // set PF1 to be a digital pin
+   GPIO_PORTF_DIR |= 0x04; // set PF1 to be an output pin
+   GPIO_PORTF_AFSEL &= ~0x04; // disable alternate functions for PF1
+   GPIO_PORTF_DATA |= 0x04;
+
+   // Configuration for Green LED
+   GPIO_PORTF_DEN |= 0x08;  // set PF1 to be a digital pin
+   GPIO_PORTF_DIR |= 0x08; // set PF1 to be an output pin
+   GPIO_PORTF_AFSEL &= ~0x08; // disable alternate functions for PF1
+   GPIO_PORTF_DATA |= 0x08;
+
+    // Configuration for SW1
+    GPIO_PORTF_DEN |= 0x10;  // set PF4 to be a digital pin
+    GPIO_PORTF_DIR &= ~0x10; // set PF4 to be an output pin
+    GPIO_PORTF_AFSEL &= ~0x10; // disable alternate functions for PF4
+    GPIO_PORTF_PUR |= 0x10;
+}
+void delay (void)
+{
+    int i = 0;
+    for (i = 0; i <=1000000; i = i + 1)
+    {
+
+    }
+}
+
+int main(void)
+{
+    gpio_portf_init();
+    while (1)
+    {
+
+       GPIO_PORTF_DATA ^= 0x02;
+       delay();
+    }
+}
