4efba6068cd49e75364968e5870e55ef93532471
Parazyt <113295033+Parzyt-bit@users.noreply.github.com>
2023-09-25 22:45:58 -0600

    Added comments for Final_programm

--- Cme331_lab1.c
+++ Cme331_lab1.c
@@ -1,15 +1,15 @@
 
-#define SYSCTL_RCGC2   (* ((volatile unsigned long *) 0x400FE108))  // 0x400FE108 is a 'memory address' of a register that has particular function in the 'microcontroller'
-#define GPIO_PORTF_DEN (* ((volatile unsigned long *) 0x4002551C))
-#define GPIO_PORTF_DIR (* ((volatile unsigned long *) 0x40025400))
-#define GPIO_PORTF_AFSEL (* ((volatile unsigned long *) 0x40025420))
-#define GPIO_PORTF_DATA (* ((volatile unsigned long *) 0x400253FC))
-#define GPIO_PORTF_PUR (* ((volatile unsigned long *) 0x40025510))
+#define SYSCTL_RCGC2       (* ((volatile unsigned long *) 0x400FE108)) 
+#define GPIO_PORTF_DEN     (* ((volatile unsigned long *) 0x4002551C))
+#define GPIO_PORTF_DIR     (* ((volatile unsigned long *) 0x40025400))
+#define GPIO_PORTF_AFSEL   (* ((volatile unsigned long *) 0x40025420))
+#define GPIO_PORTF_DATA    (* ((volatile unsigned long *) 0x400253FC))
+#define GPIO_PORTF_PUR     (* ((volatile unsigned long *) 0x40025510))
 
 
 void gpio_portf_init(void)
 {
-    // This line sets the second bit (bit 3) high in the register at location 0x400FE108
+    // This line sets the sixth bit (bit 5) high in the register at location 0x400FE108
     SYSCTL_RCGC2 |= 0x20; // enables 'clock' for PortF
     int delay = SYSCTL_RCGC2;
 
@@ -17,29 +17,28 @@ void gpio_portf_init(void)
     GPIO_PORTF_DEN |= 0x02;  // set PF1 to be a digital pin
     GPIO_PORTF_DIR |= 0x02; // set PF1 to be an output pin
     GPIO_PORTF_AFSEL &= ~0x02; // disable alternate functions for PF1
-    GPIO_PORTF_DATA |= 0x02;
+    GPIO_PORTF_DATA |= 0x02; // enable pullup resistor for PF1
 
    // Configuration for Blue LED
-    GPIO_PORTF_DEN |= 0x04;  // set PF1 to be a digital pin
-    GPIO_PORTF_DIR |= 0x04; // set PF1 to be an output pin
-    GPIO_PORTF_AFSEL &= ~0x04; // disable alternate functions for PF1
-    GPIO_PORTF_DATA |= 0x04;
+    GPIO_PORTF_DEN |= 0x04;  // set PF2 to be a digital pin
+    GPIO_PORTF_DIR |= 0x04; // set PF2 to be an output pin
+    GPIO_PORTF_AFSEL &= ~0x04; // disable alternate functions for PF2
+    GPIO_PORTF_DATA |= 0x04; // enable pullup resistor for PF2
 
     // Configuration for Green LED
-    GPIO_PORTF_DEN |= 0x08;  // set PF1 to be a digital pin
-    GPIO_PORTF_DIR |= 0x08; // set PF1 to be an output pin
-   GPIO_PORTF_AFSEL &= ~0x08; // disable alternate functions for PF1
-    GPIO_PORTF_DATA |= 0x08;
+    GPIO_PORTF_DEN |= 0x08;  // set PF3 to be a digital pin
+    GPIO_PORTF_DIR |= 0x08; // set PF3 to be an output pin
+   GPIO_PORTF_AFSEL &= ~0x08; // disable alternate functions for PF3
+    GPIO_PORTF_DATA |= 0x08; // enable pullup resistor for PF3
 
     // Configuration for SW1
     GPIO_PORTF_DEN |= 0x10;  // set PF4 to be a digital pin
     GPIO_PORTF_DIR &= ~0x10; // set PF4 to be an output pin
     GPIO_PORTF_AFSEL &= ~0x10; // disable alternate functions for PF4
-    GPIO_PORTF_PUR |= 0x10;
+    GPIO_PORTF_PUR |= 0x10; // enable pullup resistor for PF4
 }
 void delay (void)
 {
-
     int i = 0;
     while (i <= 800000)
     {
@@ -47,11 +46,10 @@ void delay (void)
         {
             i = i + 1;
         }
-        else                        // Switch is pressed
+        else      // Switch is pressed
         {
             break;
         }
-
     }
 }
 
@@ -67,14 +65,30 @@ void DELAY (void)
 int main(void)
 {
     gpio_portf_init();
-    while (1)
+    
+    // Main loop
+    while (1) 
     {
-        GPIO_PORTF_DATA &= ~0x04;
-        GPIO_PORTF_DATA &= ~0x08;
-
-        while (1)
+        GPIO_PORTF_DATA &= ~0x04;  // Turning off the Blue LED if switched ON
+        GPIO_PORTF_DATA &= ~0x08;  // Turning off the Green LED if switched ON
+        
+        while (1)  // internal loop 1, for blinking Red LED
         {
-            GPIO_PORTF_DATA ^= 0x02;
+            GPIO_PORTF_DATA ^= 0x02;  // Toggles the Red LED infinitely
+            delay();
+            if ((GPIO_PORTF_DATA & 0x10) == 0)
+            {
+                break;   
+            }
+        }
+        DELAY ();
+        
+        GPIO_PORTF_DATA &= ~0x02;  // Turning off the Red LED if switched ON
+        GPIO_PORTF_DATA &= ~0x08;  // Turning off the Green LED if switched ON
+        
+        while (1)  // internal loop 2, for blinking Blue LED
+        {
+            GPIO_PORTF_DATA ^= 0x04;  // Toggles the Blue LED infinitely
             delay();
             if ((GPIO_PORTF_DATA & 0x10) == 0)
             {
@@ -82,24 +96,13 @@ int main(void)
             }
         }
         DELAY ();
-        GPIO_PORTF_DATA &= ~0x02;
-        GPIO_PORTF_DATA &= ~0x08;
-        while (1)
-        {
-            GPIO_PORTF_DATA ^= 0x04;
-            delay();
-            if ((GPIO_PORTF_DATA & 0x10) == 0)
-            {
-                break;
-            }
-        }
-        DELAY ();
 
-        GPIO_PORTF_DATA &= ~0x02;
-        GPIO_PORTF_DATA &= ~0x04;
-        while (1)
+        GPIO_PORTF_DATA &= ~0x02;  // Turning off the Red LED if switched ON
+        GPIO_PORTF_DATA &= ~0x04;  // Turning off the Blue LED if switched ON
+        
+        while (1)  // internal loop 3, for blinking Green LED
         {
-            GPIO_PORTF_DATA ^= 0x08;
+            GPIO_PORTF_DATA ^= 0x08;  // Toggles the Green LED infinitely
             delay();
             if ((GPIO_PORTF_DATA & 0x10) == 0)
             {
